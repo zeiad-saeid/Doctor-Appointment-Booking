0e9dd076a2ecb085d5dc9e723fdd08336d90f579
zeiad-saeid <z3719258@gmail.com>
2024-12-05 16:07:11 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,50 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import {assets} from "../assets/assets_frontend/assets.js"
+import { useState } from "react"
 const Navbar = () => {
+
+const navigate = useNavigate()
+const [showMenu, setShowMenu] = useState(false)
+const [token, setToken] = useState(true)
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-        <img src={assets.logo} alt="logo" />
+        <img className="w-44 cursor-pointer" src={assets.logo} alt="logo" />
 
-        <ul>
-            <NavLink>
-                <li>HOME</li>
-                <hr />
+        <ul className="hidden md:flex items-start gap-5 font-medium">
+            <NavLink to="/">
+                <li className="py-1">HOME</li>
+                <hr className="hidden border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
             </NavLink>
-            <NavLink>
-                <li>ALL DOCTORS</li>
-                <hr />
+            <NavLink  to="/doctors">
+                <li className="py-1">ALL DOCTORS</li>
+                <hr className="hidden border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
             </NavLink>
-            <NavLink>
-                <li>ABOUT</li>
-                <hr />
+            <NavLink   to="/about">
+                <li className="py-1">ABOUT</li>
+                <hr className="hidden border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
             </NavLink>
-            <NavLink>
-                <li>Contact</li>
-                <hr />
+            <NavLink  to="/contact">
+                <li className="py-1">CONTACT</li>
+                <hr className="hidden border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
             </NavLink>
         </ul>
-        <div className="">
-             <button>Create account</button>
+        <div className="flex items-center gap-4">
+            {
+                token?<div className="flex items-center cursor-pointer gap-2 group relative">
+                    <img className="w-8 rounded-full" src={assets.profile_pic} alt="user_profile_pic" />
+                    <img className="w-2.5 " src={assets.dropdown_icon} alt="dropdown_icon" />
+                    <div>
+                         <div>
+                            <p>My Profile</p>
+                            <p>My Appointments</p>
+                            <p>Logout</p>
+                         </div>
+                    </div>
+                </div>
+                : 
+                <button onClick={()=>navigate('/login')} className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block">Create account</button>
+            }
+            
         </div>
     </div>
   )
